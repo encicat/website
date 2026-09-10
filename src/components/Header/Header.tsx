ae628b6e4fd66d4b2102d271c11137a4b43e8fde
Javier López Úbeda <jlopezcur@gmail.com>
2026-09-10 17:55:10 +0200

    style: reorder the main navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,10 +11,10 @@ interface Props {
 }
 
 const links = [
-  { href: '/quienes-somos', label: 'Quiénes somos' },
   { href: '/adopciones', label: 'Adopciones' },
   { href: '/ayudanos', label: 'Ayúdanos' },
   { href: '/adoptados', label: 'Adoptados' },
+  { href: '/quienes-somos', label: 'Quiénes somos' },
   { href: '/contacto', label: 'Contacto' },
 ];
 
