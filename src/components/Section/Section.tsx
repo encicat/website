a7687f2eb7e82f6e11cfc8980644737061147c34
Javier López Úbeda <jlopezcur@gmail.com>
2026-09-10 17:52:01 +0200

    style: reduce the gap between headings and content

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -31,6 +31,6 @@ export const Section: React.FC<Props> = ({
         }}
       />
     )}
-    <div className="max-w-5xl mx-auto py-20">{children}</div>
+    <div className="max-w-5xl mx-auto pt-10 pb-20">{children}</div>
   </div>
 );
