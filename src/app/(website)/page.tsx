332a4a8a13f9f1b24e011e58731375286d8ebf19
Javier López Úbeda <jlopezcur@gmail.com>
2026-09-10 17:37:14 +0200

    feat: update the site tagline

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -7,9 +7,11 @@ import { DonationItem } from '@/src/components/DonationItem';
 import { Section } from '@/src/components/Section';
 import { SectionTitle } from '@/src/components/SectionTitle';
 import { Slider } from '@/src/components/Slider';
+import { getCachedSettings } from '@/src/helpers/cached';
 import { reader } from '@/src/helpers/reader';
 
 export default async function HomePage() {
+  const settings = await getCachedSettings();
   const posts = (await reader.collections.posts.all())
     .sort((a, b) =>
       compareDesc(a?.entry?.publishedAt ?? '', b?.entry?.publishedAt ?? ''),
@@ -32,7 +34,7 @@ export default async function HomePage() {
 
   return (
     <main>
-      <Slider />
+      <Slider tagline={settings.slogan} />
       {home_page?.adoption_show === 'yes' && (
         <Section>
           <SectionTitle
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,4 +1,8 @@
-export const Slider: React.FC = () => (
+interface Props {
+  tagline?: string;
+}
+
+export const Slider: React.FC<Props> = ({ tagline = '' }) => (
   <div
     className="block relative border-b-gray-100 border-b"
     style={{
@@ -8,9 +12,7 @@ export const Slider: React.FC = () => (
   >
     <div className="relative z-20 flex flex-col items-center justify-center h-full px-4 text-center">
       <div className="text-4xl md:text-7xl">Bienvenido a EnciCat</div>
-      <div className="text-lg md:text-2xl">
-        Rescatamos gatos y les buscamos una familia responsable
-      </div>
+      <div className="text-lg md:text-2xl">{tagline}</div>
     </div>
   </div>
 );
