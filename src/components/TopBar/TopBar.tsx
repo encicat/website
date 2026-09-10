aaae689e52e90a5fc93ac0d6d23338b8c91532d5
Javier López Úbeda <jlopezcur@gmail.com>
2026-09-10 14:51:12 +0200

    feat: show only contact icons in the top bar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -17,14 +17,12 @@ export const TopBar: React.FC<Props> = ({
   <div className="w-full py-2 px-5 bg-green-800 text-white">
     <div className="max-w-5xl m-auto flex flex-wrap items-center justify-between gap-x-4 gap-y-1">
       <div className="flex flex-wrap items-center gap-x-4">
-        <a href={`mailto:${email}`} className="flex items-center">
-          <Mail className="mr-2" />
-          {email}
+        <a href={`mailto:${email}`} aria-label="Escríbenos" className="flex">
+          <Mail />
         </a>
         {phone !== '' && (
-          <a href={toTelHref(phone)} className="flex items-center">
-            <Phone className="mr-2" />
-            {phone}
+          <a href={toTelHref(phone)} aria-label="Llámanos" className="flex">
+            <Phone />
           </a>
         )}
       </div>
