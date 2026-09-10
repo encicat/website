4b8994050b8a2e76b3df4a6324fb5cedcf862278
Javier López Úbeda <jlopezcur@gmail.com>
2026-09-10 14:20:56 +0200

    style: center and scale home hero text on mobile

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -6,9 +6,9 @@ export const Slider: React.FC = () => (
       backgroundImage: 'url(/images/bg.png)',
     }}
   >
-    <div className="relative z-20 flex flex-col items-center justify-center h-full px-4">
-      <div className="text-7xl">Bienvenido a EnciCat</div>
-      <div className="text-2xl">
+    <div className="relative z-20 flex flex-col items-center justify-center h-full px-4 text-center">
+      <div className="text-4xl md:text-7xl">Bienvenido a EnciCat</div>
+      <div className="text-lg md:text-2xl">
         Tu protectora de confianza en nuestro ámbito territorial
       </div>
     </div>
