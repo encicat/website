8cebe3a801ad602a6962b6eb7e1eee0a30f7ad9c
Javier López Úbeda <jlopezcur@gmail.com>
2026-09-11 23:45:25 +0200

    feat: add a hero background overlay for contrast

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,12 +11,15 @@ export const Hero: React.FC<Props> = ({
 }) => (
   <div className="w-full block px-6 py-12 md:p-12 relative shadow-[0_4px_6px_-1px_rgba(0,0,0,0.05)]">
     {bgImgSrc && (
-      <div
-        className={`absolute inset-0 -z-10 ${bgFixed ? 'md:bg-fixed' : ''}`}
-        style={{
-          backgroundImage: `url(${bgImgSrc})`,
-        }}
-      />
+      <>
+        <div
+          className={`absolute inset-0 -z-10 ${bgFixed ? 'md:bg-fixed' : ''}`}
+          style={{
+            backgroundImage: `url(${bgImgSrc})`,
+          }}
+        />
+        <div className="absolute inset-0 -z-10 bg-white/80" />
+      </>
     )}
 
     <div className="max-w-5xl mx-auto">{children}</div>
